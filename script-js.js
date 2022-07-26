6488c90264d4a6d16d29dce95b6b6d12267fda16
George <georgea.fotheringham@gmail.com>
2022-07-26 19:32:18 -0400

    create 16x16 grid of squares

---
+++ script-js.js
@@ -0,0 +1,13 @@
+
+const container = document.querySelector('div.container');
+
+
+for(i=0; i < 256; i++) {
+const square = document.createElement('div');
+document.createAttribute('square');
+square.classList.add('square');
+container.appendChild(square);
+}
+
+
+
