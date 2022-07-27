5d0d4ab4e9476e9919e13640668d05623a40c1d8
George <georgea.fotheringham@gmail.com>
2022-07-26 20:08:33 -0400

    fixing type typo

--- script-js.js
+++ script-js.js
@@ -7,11 +7,26 @@ const square = document.createElement('div');
 document.createAttribute('square');
 square.classList.add('square');
 container.appendChild(square);
-square.addEventListener('onHover', mouseDraw)
+square.addEventListener('mouseover', mouseDraw)
+
+
+function generateGrid() {
+    console.log(square)
+    container.removeChild(square)
+}
 
 }
 
-
 function mouseDraw() {
     this.setAttribute('style', 'background-color:white;')
+}
+
+const resetButton = document.querySelector('button');
+
+resetButton.addEventListener('click', generateGrid)
+
+function generateGrid(square, container) {
+    console.log(square)
+    for(square in container)
+    container.removeChild('square')
 }
