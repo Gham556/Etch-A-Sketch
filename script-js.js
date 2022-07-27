0211d609ec7fda4189d4c186405f4a2c3892809e
George <georgea.fotheringham@gmail.com>
2022-07-26 21:30:56 -0400

    prompt input spawns number of boxes, need to find way to change box size according to gridSize

--- script-js.js
+++ script-js.js
@@ -4,17 +4,34 @@ const container = document.querySelector('div.container');
 
 for(i=0; i < 256; i++) {
 const square = document.createElement('div');
-document.createAttribute('square');
+//document.createAttribute('square');
 square.classList.add('square');
 container.appendChild(square);
 square.addEventListener('mouseover', mouseDraw)
 
 
 function generateGrid() {
-   const myDiv = document.getElementById('main');
+//removes existing grid 
+    const myDiv = document.getElementById('main');
    myDiv.innerHTML = "";
-}
 
+  const gridSize = prompt("Choose Your Grid Size With A Single Number Below 100")
+ 
+  const squareSize = (896 / gridSize) - 6 / 2;  
+  console.group(squareSize);
+  
+  
+  for (let i = 0; i < gridSize * gridSize; i++){
+    const square = document.createElement('div');
+    document.createAttribute('square');
+    square.classList.add('square');
+    const squareStyle = document.querySelector('div').style
+   squareStyle.padding = squareSize,'px';
+    container.appendChild(square);
+    square.addEventListener('mouseover', mouseDraw)
+  } 
+
+}
 }
 
 function mouseDraw() {
